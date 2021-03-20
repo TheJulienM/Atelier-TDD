9a6a25a8869373f857b90e3690763118c23fca50
TheJulienM <julien.marfella77@gmail.com>
2021-03-20 14:17:08 +0100

    DEBUG test_Task.py wasn't updated

--- test_Task.py
+++ test_Task.py
@@ -4,7 +4,7 @@ def test_createTask():
     id = 0
     title = "Title 0"
     status = None
-    task_0 = Task.Task(id,title,status)
+    task_0 = Task.Task(id,title)
     assert task_0.id == 0
     assert task_0.title == "Title 0"
     assert task_0.status == None
@@ -13,7 +13,7 @@ def test_changeStatusToDo():
     id = 1
     title = "Title 1"
     status = None
-    task_1 = Task.Task(id,title,status)
+    task_1 = Task.Task(id,title)
     task_1.changeStatusToDo()
     assert task_1.status == "toDo"
 
@@ -21,7 +21,7 @@ def test_changeStatusDone():
     id = 2
     title = "Title 2"
     status = None
-    task_2 = Task.Task(id,title,status)
+    task_2 = Task.Task(id,title)
     task_2.changeStatusToDone()
     assert task_2.status == "Done"
 
